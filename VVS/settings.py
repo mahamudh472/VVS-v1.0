3093f930b6b9fb98beac6b5ecfb4f4eb29a2282a
= <=>
2023-12-30 23:12:11 +0600

    init

--- VVS/settings.py
+++ VVS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-50^=4hda*u*fed=h!o57#80-22al5^##xz_8*dqkvue#sq@ja8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'VVS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'Nf98lFxLUOmC',
+        'HOST': 'ep-ancient-sea-806661-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
